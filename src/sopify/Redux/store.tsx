c36d3c3b276b0e7c9b4e3793ab54164af90c70c6
RAJU BARDE <125546485+rajuforbear@users.noreply.github.com>
2023-07-21 18:06:31 +0530

    version upgader

--- src/sopify/Redux/store.tsx
+++ src/sopify/Redux/store.tsx
@@ -2,13 +2,14 @@ import createSagaMiddleware from 'redux-saga';
 import {configureStore} from '@reduxjs/toolkit';
 import MySlice from './Slice';
 import Saga from './saga';
-
+import { createLogger } from 'redux-logger'
+const logger=createLogger()
 const saga = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     data: MySlice,
   },
-  middleware: [saga],
+  middleware: [saga,logger],
 });
 saga.run(Saga);
 export type RootState = ReturnType<typeof store.getState>
