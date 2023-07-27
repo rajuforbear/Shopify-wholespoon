933973ead3db03aaa82981528fff456a58e39932
RAJU BARDE <125546485+rajuforbear@users.noreply.github.com>
2023-07-27 10:32:27 +0530

    some changes

---
+++ src/screens/Auth/Internet.tsx
@@ -0,0 +1,23 @@
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import { StackScreenProps } from '@react-navigation/stack';
+import { NavigationParams } from '../../navigation';
+ type Props=StackScreenProps<NavigationParams,'Internet'>
+const Internet:React.FC<Props> = ({navigation}) => {
+  const betInfo = useNetInfo();
+  
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: wp(4), fontWeight: '700',fontStyle:'italic'}}>
+        Looks like you are not connected to the internet !
+      </Text>
+    </View>
+  );
+};
+
+export default Internet;
